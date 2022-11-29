8fdf1aa02157c9dc12981b4f092fa14863f5d351
Tim Burt <tim@biffbangpow.com>
2022-11-29 15:51:38 +0000

    Update build tools Add CSS

--- src/Control/ContactMapElementController.php
+++ src/Control/ContactMapElementController.php
@@ -2,10 +2,15 @@
 
 namespace BiffBangPow\Element\Control;
 
-class ContactMapElementController
+use DNADesign\Elemental\Controllers\ElementController;
+use SilverStripe\View\Requirements;
+
+class ContactMapElementController extends ElementController
 {
     protected function init()
     {
         parent::init();
+        Requirements::javascript('biffbangpow/silverstripe-contactmap-element:client/dist/javascript/osm.js', ['type' => false]);
+        Requirements::css('biffbangpow/silverstripe-contactmap-element:client/dist/css/osm.js');
     }
 }
