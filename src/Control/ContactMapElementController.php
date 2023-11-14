adcddc7962944cf8189508b05d2d3301c732bbb6
Tim Burt <tim@biffbangpow.com>
2023-11-14 11:00:07 +0000

    Make OSM the default map type, even if not set in config

--- src/Control/ContactMapElementController.php
+++ src/Control/ContactMapElementController.php
@@ -12,15 +12,15 @@ class ContactMapElementController extends ElementController
     {
         parent::init();
         $provider = SiteConfig::current_site_config()->MapsProvider;
-        if ($provider === 'osm') {
-            Requirements::javascript('biffbangpow/silverstripe-contactmap-element:client/dist/javascript/osm.js', ['type' => false]);
-            Requirements::css('biffbangpow/silverstripe-contactmap-element:client/dist/css/osm.css');
-        } else if ($provider === 'google') {
+        if ($provider === 'google') {
             $mapsKey = SiteConfig::current_site_config()->GoogleKey;
-            if ($mapsKey != "") {                
+            if ($mapsKey != "") {
                 Requirements::javascriptTemplate('biffbangpow/silverstripe-contactmap-element:client/dist/javascript/gmaps.js', ['KEY' => $mapsKey]);
                 Requirements::css('biffbangpow/silverstripe-contactmap-element:client/dist/css/gmap.css');
             }
+        } else {
+            Requirements::javascript('biffbangpow/silverstripe-contactmap-element:client/dist/javascript/osm.js', ['type' => false]);
+            Requirements::css('biffbangpow/silverstripe-contactmap-element:client/dist/css/osm.css');
         }
     }
 }
